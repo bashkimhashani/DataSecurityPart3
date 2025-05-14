40fcf5a8e0336180cc3855fc7baeb4b41cd782ff
bashkimhashani <bashkim.hashani@student.uni-pr.edu>
2025-05-14 19:16:05 +0200

    GridPane & Buttons

--- src/main/java/com/example/datasecuritypart3/CVCipher.java
+++ src/main/java/com/example/datasecuritypart3/CVCipher.java
@@ -1,8 +1,10 @@
 package com.example.datasecuritypart3;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.stage.*;
 import javafx.scene.control.*;
 
@@ -21,5 +23,17 @@ public class CVCipher extends Application {
     @Override
     public void start(Stage stage) {
         stage.setTitle("Caesar & Vigenère Cipher Tool");
+
+        GridPane topPane = new GridPane();
+        topPane.setHgap(10);
+        topPane.setVgap(10);
+        topPane.setPadding(new Insets(10));
+
+        topPane.add(new Label("Key:"), 0, 0);
+        topPane.add(keyField, 1, 0);
+
+        Button browseFileBtn = new Button("Browse");
+        Button saveFileVtn = new Button("Save");
+
     }
 }

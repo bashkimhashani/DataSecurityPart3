8bffd5997591a551b4f1d28c5afed5f5d362ac9f
Dua-Gashi <dua.gashi@student.uni-pr.edu>
2025-05-17 17:10:43 +0200

    Perdorimi i librarive

--- src/main/java/com/example/datasecuritypart3/CVCipher.java
+++ src/main/java/com/example/datasecuritypart3/CVCipher.java
@@ -2,9 +2,14 @@ package com.example.datasecuritypart3;
 
 import javafx.application.Application;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.*;
 import javafx.scene.control.*;
 
@@ -96,7 +101,6 @@ public class CVCipher extends Application {
             } else {
                 inputText = messageArea.getText();
             }
-
         }
 
 
